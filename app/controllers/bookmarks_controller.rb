013e6a0e4593ea2c84586150e3251d20e2da2527
kvadrats <emils.kantans@gmail.com>
2016-04-01 16:23:17 +0300

    added ability to create list names for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,27 @@
 class BookmarksController < ApplicationController
+
+  def index
+  	@bookmarks = Bookmark.all
+  end
+
+  def new
+	@bookmark = Bookmark.new
+  end
+
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to bookmarks_url
+    else
+      render :new
+    end
+  end
+
+  def bookmark_params
+  	params.require(:bookmark).permit(:list, :user_id)
+  end
+
+
 end
