dbc8b3187217a358aae7d73cc44761934318ab08
Kvadrats <emils.kantans@gmail.com>
2016-05-02 18:05:19 -0700

    fixed up comments, with a bit of design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
     end
     @journal_articles = @post.journal_articles
     @research_papers = @post.research_papers
-    @comments = Comment.where(params[:post_id])#.order(:created_at)
+    @comments = Comment.where(post_id: params[:id])
     @comment = Comment.new(parent_id: params[:parent_id])
 
     @allcategories = Category.all
